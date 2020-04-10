fc13006f53b66c202973272d5bc93c3b4bde423c
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-04-10 16:42:25 -0500

    Update Part B

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,13 +1,41 @@
-from random import randint
+# Part A
+# from random import randint
 
-randVal = randint(1,100)
-while(True):    # while(True==True):
-    guess = int(input("Input a number: "))
+# randVal = randint(1,100)
+# while(True):    # while(True==True):
+#     guess = int(input("Input a number: "))
+#     if guess == randVal:
+#         print("Ganaste")
+#         break
+#     elif guess < randVal:
+#         print("Tu numero debe ser mas alto")
+#     else:
+#         print("Tu numero debe ser mas bajo")
+# print("La respuesta correcta es: ", randVal)
+
+
+
+# Part B
+from random import random
+from time import clock
+
+randVal = random()
+print(randVal)
+
+upper = 1.0
+lower = 0.0
+guess = 0.5
+startTime = clock()
+while(True):
+    guess = (lower + upper) / 2
     if guess == randVal:
-        print("Ganaste")
         break
     elif guess < randVal:
-        print("Tu numero debe ser mas alto")
+        lower = guess
     else:
-        print("Tu numero debe ser mas bajo")
-print("La respuesta correcta es: ", randVal)
+        upper = guess
+endTime = clock()
+print(guess)
+print("Tomó: ",endTime-startTime," seconds")
+
+    
