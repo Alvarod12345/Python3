45de686da740a272daeafaaf202dea3cfa709c33
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-04-09 22:38:02 -0500

    libraries math n time

---
+++ libraryTime.py
@@ -0,0 +1,36 @@
+# dev 4lv4r0d
+# time n math library
+import time
+import math
+
+# currentTime = time.clock()
+
+# print(currentTime)
+
+# print("hi")
+# time.sleep(5)
+# print("hi 2 ")
+
+# for i in range(1,11):
+#     time.sleep(i)
+#     print(i)
+
+Val = 3
+sqrtVal = math.sqrt(Val)
+print(sqrtVal)
+print(sqrtVal**2)
+
+expVal = math.exp(Val)
+print(expVal)
+
+factVal = math.factorial(Val)
+print(factVal)
+
+sinVal = math.sin(Val)
+print(sinVal)
+
+floorVal = math.floor(Val)
+print(floorVal)
+
+ceilingValue = math.ceil(Val)
+print(ceilingValue)
