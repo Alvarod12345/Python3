f3c04d01d1a2684c45b99cf465165b812101f7f5
root <alvarosantisteban56@gmail.com>
2020-04-22 00:07:12 -0500

    MARCH22

---
+++ class18.py
@@ -0,0 +1,48 @@
+# Error Handling
+
+keyword = "helloworld"
+
+# ex1
+# try:
+#     print(int(keyword))
+# except:
+#     print("got a value error")
+
+
+# ex2 using exception
+# try:
+#     print(int(keyword))
+
+# except Exception as e:
+#     print(e)
+
+# ex3
+# try:
+#     print(int(keyword))
+# except ValueError:
+#     print("got a value error")
+# except:
+#     print("other except")
+# finally:
+#     print("finally")
+
+
+# ex4
+try:
+    # raise ValueError
+    # raise ZeroDivisionError
+    # raise AttributeError
+    raise NameError("Error 1")
+
+except ValueError:
+    print("you got a value error")
+except ZeroDivisionError:
+    print("zero division error")
+except AttributeError:
+    print("you got a atribute error")
+    
+except Exception as x:
+    print("except")
+    print(str(x))
+finally:
+    print("finally")
--- test.py
+++ test.py
@@ -326,222 +326,222 @@ class Team:
 
 
 
-import random
+# import random
 
-def main():
-    welcome = ['Welcome to Hangman! A word will be chosen at random and',
-               'you must try to guess the word correctly letter by letter',
-               'before you run out of attempts. Good luck!'
-               ]
+# def main():
+#     welcome = ['Welcome to Hangman! A word will be chosen at random and',
+#                'you must try to guess the word correctly letter by letter',
+#                'before you run out of attempts. Good luck!'
+#                ]
 
-    for line in welcome:
-        print(line, sep='\n')
+#     for line in welcome:
+#         print(line, sep='\n')
 
-    # setting up the play_again loop
+#     # setting up the play_again loop
 
-    play_again = True
+#     play_again = True
 
-    while play_again:
-        # set up the game loop
+#     while play_again:
+#         # set up the game loop
 
-        words = ["hangman", "chairs", "backpack", "bodywash", "clothing",
-                 "computer", "python", "program", "glasses", "sweatshirt",
-                 "sweatpants", "mattress", "friends", "clocks", "biology",
-                 "algebra", "suitcase", "knives", "billionare", "shampoo"
-                 ]
+#         words = ["hangman", "chairs", "backpack", "bodywash", "clothing",
+#                  "computer", "python", "program", "glasses", "sweatshirt",
+#                  "sweatpants", "mattress", "friends", "clocks", "biology",
+#                  "algebra", "suitcase", "knives", "billionare", "shampoo"
+#                  ]
 
-        chosen_word = random.choice(words).lower()
-        player_guess = None # will hold the players guess
-        guessed_letters = [] # a list of letters guessed so far
-        word_guessed = []
-        for letter in chosen_word:
-            word_guessed.append("-") # create an unguessed, blank version of the word
-        joined_word = None # joins the words in the list word_guessed
+#         chosen_word = random.choice(words).lower()
+#         player_guess = None # will hold the players guess
+#         guessed_letters = [] # a list of letters guessed so far
+#         word_guessed = []
+#         for letter in chosen_word:
+#             word_guessed.append("-") # create an unguessed, blank version of the word
+#         joined_word = None # joins the words in the list word_guessed
 
-        HANGMAN = (
-"""
------
-|   |
-|
-|
-|
-|
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-|
-|
-|
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-|  -+-
-|
-|
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-
-|
-|
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|
-|
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|   | 
-|
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|   | 
-|   | 
-|
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|   | 
-|   | 
-|  |
-|
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|   | 
-|   | 
-|  | 
-|  | 
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|   | 
-|   | 
-|  | | 
-|  | 
-|
---------
-""",
-"""
------
-|   |
-|   0
-| /-+-\ 
-|   | 
-|   | 
-|  | | 
-|  | | 
-|
---------
-""")
+#         HANGMAN = (
+# """
+# -----
+# |   |
+# |
+# |
+# |
+# |
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# |
+# |
+# |
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# |  -+-
+# |
+# |
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-
+# |
+# |
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |
+# |
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |   | 
+# |
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |   | 
+# |   | 
+# |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |   | 
+# |   | 
+# |  |
+# |
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |   | 
+# |   | 
+# |  | 
+# |  | 
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |   | 
+# |   | 
+# |  | | 
+# |  | 
+# |
+# --------
+# """,
+# """
+# -----
+# |   |
+# |   0
+# | /-+-\ 
+# |   | 
+# |   | 
+# |  | | 
+# |  | | 
+# |
+# --------
+# """)
 
-        print(HANGMAN[0])
-        attempts = len(HANGMAN) - 1
+#         print(HANGMAN[0])
+#         attempts = len(HANGMAN) - 1
 
 
-        while (attempts != 0 and "-" in word_guessed):
-            print(("\nYou have {} attempts remaining").format(attempts))
-            joined_word = "".join(word_guessed)
-            print(joined_word)
+#         while (attempts != 0 and "-" in word_guessed):
+#             print(("\nYou have {} attempts remaining").format(attempts))
+#             joined_word = "".join(word_guessed)
+#             print(joined_word)
 
-            try:
-                player_guess = str(input("\nPlease select a letter between A-Z" + "\n> ")).lower()
-            except: # check valid input
-                print("That is not valid input. Please try again.")
-                continue                
-            else: 
-                if not player_guess.isalpha(): # check the input is a letter. Also checks an input has been made.
-                    print("That is not a letter. Please try again.")
-                    continue
-                elif len(player_guess) > 1: # check the input is only one letter
-                    print("That is more than one letter. Please try again.")
-                    continue
-                elif player_guess in guessed_letters: # check it letter hasn't been guessed already
-                    print("You have already guessed that letter. Please try again.")
-                    continue
-                else:
-                    pass
+#             try:
+#                 player_guess = str(input("\nPlease select a letter between A-Z" + "\n> ")).lower()
+#             except: # check valid input
+#                 print("That is not valid input. Please try again.")
+#                 continue                
+#             else: 
+#                 if not player_guess.isalpha(): # check the input is a letter. Also checks an input has been made.
+#                     print("That is not a letter. Please try again.")
+#                     continue
+#                 elif len(player_guess) > 1: # check the input is only one letter
+#                     print("That is more than one letter. Please try again.")
+#                     continue
+#                 elif player_guess in guessed_letters: # check it letter hasn't been guessed already
+#                     print("You have already guessed that letter. Please try again.")
+#                     continue
+#                 else:
+#                     pass
 
-            guessed_letters.append(player_guess)
+#             guessed_letters.append(player_guess)
 
-            for letter in range(len(chosen_word)):
-                if player_guess == chosen_word[letter]:
-                    word_guessed[letter] = player_guess # replace all letters in the chosen word that match the players guess
+#             for letter in range(len(chosen_word)):
+#                 if player_guess == chosen_word[letter]:
+#                     word_guessed[letter] = player_guess # replace all letters in the chosen word that match the players guess
 
-            if player_guess not in chosen_word:
-                attempts -= 1
-                print(HANGMAN[(len(HANGMAN) - 1) - attempts])
+#             if player_guess not in chosen_word:
+#                 attempts -= 1
+#                 print(HANGMAN[(len(HANGMAN) - 1) - attempts])
 
-        if "-" not in word_guessed: # no blanks remaining
-            print(("\nCongratulations! {} was the word").format(chosen_word))
-        else: # loop must have ended because attempts reached 0
-            print(("\nUnlucky! The word was {}.").format(chosen_word))
+#         if "-" not in word_guessed: # no blanks remaining
+#             print(("\nCongratulations! {} was the word").format(chosen_word))
+#         else: # loop must have ended because attempts reached 0
+#             print(("\nUnlucky! The word was {}.").format(chosen_word))
 
-        print("\nWould you like to play again?")
+#         print("\nWould you like to play again?")
 
-        response = input("> ").lower()
-        if response not in ("yes", "y"):
-            play_again = False
+#         response = input("> ").lower()
+#         if response not in ("yes", "y"):
+#             play_again = False
 
-if __name__ == "__main__":
-    main()  
+# if __name__ == "__main__":
+#     main()  
 
 
 
@@ -730,3 +730,74 @@ if __name__ == "__main__":
 
 
 # main()
+
+
+
+
+
+
+
+# example erro handling
+
+def compare(a = None, b = None, c = None):
+    try:
+        value = False
+        a = int(a)
+        b = int(b)       
+        c = int(c)   
+        print("Valid values.")     
+        if a == b or a == c or b == c:
+            value = True
+    except ValueError:
+        print("ValueError: Only, numbers are allowed.")
+    except TypeError:
+        print("TypeError: Please enter only numbers.")
+    except Exception:
+        print(Exception)
+        print("Exception, Only numbers are allowed!")
+    finally:
+        print("Values ", a, b, c)
+        return value
+
+# all integers
+print("Result: ",compare(1, 2, 3))
+print("\n")
+
+# numbers in str
+print("Result: ",compare("2", "3", "4"))
+print("\n")
+
+# alphabets
+print("Result: ",compare("a", "b", "c"))
+print("\n")
+
+# one boolean
+print("Result: ",compare(True))
+print("\n")
+
+# three boolean values
+print("Result: ",compare(True, True, False))
+print("\n")
+
+# integer with list
+print("Result: ",compare(1, 2, [True]))
+print("\n")
+
+# str with boolean
+print("Result: ",compare("2", "2", True))
+print("\n")
+
+# equal numbers in str
+print("Result: ",compare("2", "2", "2"))
+print("\n")
+
+# empty str
+print("Result: ",compare("-1", ""))
+print("\n")
+
+# three empty str
+print("Result: ",compare("", "", ""))
+print("\n")
+
+#floats
+print("Result: ",compare("1.1", "1","1.1"))
